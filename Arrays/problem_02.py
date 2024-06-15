50aa0fbca9bf5d54feeed4ed1f3ac3312031bc4f
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-06-15 16:19:35 +0530

    added problem 2

---
+++ Arrays/problem_02.py
@@ -0,0 +1,24 @@
+# Rotate Array
+# https://leetcode.com/problems/rotate-array/description/
+
+
+class Solution:
+    def reverse(self, nums: List[int], start: int, end: int) -> None:
+        while start < end:
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start += 1
+            end -= 1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        if k == 0:
+            return
+
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
