5ec63334ee097ec3a7eb99f6cc63a021d9b005a0
Kaushik K <kaushikk@Kaushiks-MacBook-Air.local>
2025-01-08 16:03:05 +0530

    added BFS code

--- Graphs/02_basics_code.py
+++ Graphs/02_basics_code.py
@@ -9,14 +9,50 @@ class Graph:
         else:
             print("Node already exists")
 
-    def add_edge(self, u, v, weight=1):
-        if u not in self.adjacency_list:
-            self.add_node(u)
-        if v not in self.adjacency_list:
-            self.add_node(v)
-        self.adjacency_list[u].append(v)
-        self.adjacency_list[v].append(
-            u
+    def add_edge(self, node, neighbor, weight=1):
+        if node not in self.adjacency_list:
+            self.add_node(node)
+        if neighbor not in self.adjacency_list:
+            self.add_node(neighbor)
+        self.adjacency_list[node].append(neighbor)
+        self.adjacency_list[neighbor].append(
+            node
         )  # For undirected graph; remove for directed graph
-        self.weights[(u, v)] = weight
-        self.weights[(v, u)] = weight
+        self.weights[(node, neighbor)] = weight
+        self.weights[(neighbor, node)] = weight
+
+    def print_graph(self):
+        print("The graph representation is:")
+        for node, neighbors in  self.adjacency_list.items():
+            print(node, neighbors)
+        
+    def bfs(self, start):
+        visited = []
+        result = []
+        queue = [start]
+
+        while queue:
+            node = queue.pop(0)
+            if node not in visited:
+                visited.append(node)
+                result.append(node)
+
+                for neighbor in self.adjacency_list[node]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
+        return result 
+
+
+
+graph_1 = Graph()
+graph_1.add_node(node=1)
+graph_1.add_node(node=2)
+graph_1.add_node(node=3)
+
+graph_1.add_edge(node=1, neighbor=2)
+graph_1.add_edge(node=2, neighbor=3)
+
+graph_1.print_graph()
+
+print(f"The BFS representation of the graph is: {graph_1.bfs(1)}")
